fd517a16964c7554c089c59df0fc504c8b8b2002
srikanthpragada <srikanthpragada@gmail.com>
2025-02-06 18:54:23 +0530

    06-FEB

---
+++ js/basics/strict.js
@@ -0,0 +1,10 @@
+"use strict"
+let a = 100
+
+console.log(a)
+
+price = 1000
+
+console.log(price)
+
+
---
+++ js/basics/variables.js
@@ -0,0 +1,20 @@
+var a  = 10
+
+b = 100
+console.log(a)
+console.log(typeof(a))
+console.log(b)
+
+const v = 100
+
+//v = 200
+
+a = "abc"
+console.log(typeof(a))
+
+
+
+
+
+
+
