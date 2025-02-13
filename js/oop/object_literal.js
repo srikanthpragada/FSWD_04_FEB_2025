994fadfe1bc0e8571ee617294a5da97cb38e288f
srikanthpragada <srikanthpragada@gmail.com>
2025-02-13 18:55:50 +0530

    13-FEB

---
+++ js/oop/object_literal.js
@@ -0,0 +1,17 @@
+// Object literal 
+
+let p = {
+    name: 'Scott',
+    job: 'Programmer', 
+    salary: 500000,
+    skills: ['Java', 'C#']
+}
+
+console.log(p.name, p.job, p.skills[0])
+
+p.email = "scott@gmail.com"
+p.salary = 600000
+console.log(p)
+
+
+
